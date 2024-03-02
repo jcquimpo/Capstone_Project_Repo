12d835668b2f5ac076e0d44a99f328b3c9e53948
JanPotat0 <jancquimpo@gmail.com>
2024-03-01 23:24:46 -0500

    added forms and styles to pages - basic layout basic forms

--- python_frontend/app.py
+++ python_frontend/app.py
@@ -7,16 +7,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/landing')
-def landing():
-    return render_template('landing.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
