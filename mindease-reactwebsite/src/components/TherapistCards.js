3a4d46d159db0fd334378445ae432120cd11fc49
jcquimpo <jancquimpo@gmail.com>
2024-04-07 00:51:11 -0400

    follow-up commit - example/reference code added

--- mindease-reactwebsite/src/components/TherapistCards.js
+++ mindease-reactwebsite/src/components/TherapistCards.js
@@ -13,6 +13,43 @@ import {
   MDBIcon
 } from 'mdb-react-ui-kit';
 
+
+// export default function TherapistCard({ name, role, email, phone }) {
+//   return (
+//     <MDBCard className="mb-3" style={{ borderRadius: '.75rem' }}>
+//       <MDBCardBody>
+//         <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+//           alt="Avatar" className="my-5" style={{ width: '150px' }} fluid />
+//         <MDBTypography tag="h5" className="fs-3">{name}</MDBTypography>
+//         <MDBCardText className="fs-5">{role}</MDBCardText>
+//         <MDBIcon far icon="edit mb-5" size="lg" />
+//         <MDBTypography tag="h6" className="fs-4">Information</MDBTypography>
+//         <hr className="mt-0 mb-4" />
+//         <MDBCardText className="text-muted fs-6">Email: {email}</MDBCardText>
+//         <MDBCardText className="text-muted fs-6">Phone: {phone}</MDBCardText>
+//         <div className="d-flex justify-content-start">
+//           <a href="#!" className="fs-2 me-3"><MDBIcon fab icon="facebook" size="2x" /></a>
+//           <a href="#!" className="fs-2 me-3"><MDBIcon fab icon="twitter" size="2x" /></a>
+//           <a href="#!" className="fs-2 me-3"><MDBIcon fab icon="instagram" size="2x" /></a>
+//         </div>
+//       </MDBCardBody>
+//     </MDBCard>
+//   );
+// }
+// <TherapistCard
+// name="Marie Horwitz"
+// role="Web Designer"
+// email="info@example.com"
+// phone="123 456 789"
+// />
+// <TherapistCard
+// name="John Doe"
+// role="Psychologist"
+// email="john.doe@example.com"
+// phone="987 654 321"
+// />
+
+
 export default function TherapistCards() {
   return (
     <MDBContainer className="py-5 h-100">
