c2e2ba8d8bab878d1f1f082b6f97d22b9346d0e5
JanPotat0 <jancquimpo@gmail.com>
2024-04-08 21:58:38 -0400

    commit 4/8 - minor changes made

--- mindease-reactwebsite/src/components/Navbar.js
+++ mindease-reactwebsite/src/components/Navbar.js
@@ -10,12 +10,9 @@ function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
   const location = useLocation();
-  // const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-  // const onMouseEnter = () => setDropdown(true);
-  // const onMouseLeave = () => setDropdown(false);
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
