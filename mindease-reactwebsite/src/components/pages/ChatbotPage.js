f9639b198abee00109ba497a044dbac5976bb706
jcquimpo <jancquimpo@gmail.com>
2024-04-12 18:31:23 -0400

    started chatbot page

--- mindease-reactwebsite/src/components/pages/ChatbotPage.js
+++ mindease-reactwebsite/src/components/pages/ChatbotPage.js
@@ -1,17 +1,31 @@
-import '../../App.css'
-import Footer from '../Footer';
+import React from 'react';
+// import '../../App.css';
+import '../styles/Chatbot_styles.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+// import addBtn from './public/images/add-30.jpg'
+// import msgIcon from './public/images/messageIcon.jpg'
 
 export default function ChatbotPage() {
     return (
-        <div>
-            <div class="container">
-                <div class="text-center">
-                    <h1>Welcome to EaseChat</h1>
-                    <p>Feel free to talk to us!</p>
+    <div className='chatbot-app'>
+        <div className='sidebar'>
+            <div className='upperSide'>
+                <div className='upperSideTop'><img src='' alt='' className='logo' />MindEase<i className='fab fa-typo3' /></div>
+                <button className='midBtn'><img src='' alt='' className='addBtn'/>New Chat</button>
+                <div className='upperSideBottom'>
+                    <button className='query'><img src='' alt='Query'/>What is EaseChat?</button>
+                    <button className='query'><img src='' alt='Query'/>How to use EaseChat?</button>                                        
                 </div>
             </div>
-
-            <Footer />
+            <div className='lowerSide'>
+                <div className='listItems'><img src='' alt='' className='listitemsImg'/>Home</div>
+                <div className='listItems'><img src='' alt='' className='listitemsImg'/>Saved</div>
+                <div className='listItems'><img src='' alt='' className='listitemsImg'/>Upgrade</div>
+            </div>
         </div>
+        <div className='main'>
+
+        </div>
+    </div>
     );
 }
